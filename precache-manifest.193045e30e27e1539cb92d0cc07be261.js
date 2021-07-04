c077a1f0015e0137e431d6ef4d916c41cb18a178
arnavk2319 <arnav.kaushal800@gmail.com>
2021-07-04 19:39:28 -0400

    Updates

---
+++ precache-manifest.193045e30e27e1539cb92d0cc07be261.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463b8036ddcf0193e25c7241bb407de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05f508abcd550437037b",
+    "url": "/static/css/main.9c5d4387.chunk.css"
+  },
+  {
+    "revision": "858ffa28f9541b90095e",
+    "url": "/static/js/2.404d3a32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.404d3a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f508abcd550437037b",
+    "url": "/static/js/main.44269f13.chunk.js"
+  },
+  {
+    "revision": "052fffbe62e1193bce01",
+    "url": "/static/js/runtime-main.4180024d.js"
+  },
+  {
+    "revision": "72da434e7136adf7f695529d412d0b35",
+    "url": "/static/media/Arnav_Kaushal_2020.72da434e.pdf"
+  },
+  {
+    "revision": "638436ff95a4fb3de9553e6760aa1268",
+    "url": "/static/media/coviupdate-all.638436ff.png"
+  },
+  {
+    "revision": "68bad2ff1de38ca69bc43ec0cc4abb21",
+    "url": "/static/media/dab.68bad2ff.png"
+  },
+  {
+    "revision": "031bbf49da8bad0af67306db9d7b656f",
+    "url": "/static/media/equo-all.031bbf49.png"
+  },
+  {
+    "revision": "0f24fff9f1560daf9cd6d651a19b2f7a",
+    "url": "/static/media/forkify-all.0f24fff9.png"
+  },
+  {
+    "revision": "5c4d2fd9423841eb81bbc8e4c2048a62",
+    "url": "/static/media/kova-all.5c4d2fd9.png"
+  },
+  {
+    "revision": "d63522c53821a8766b3573a4a7fa28ee",
+    "url": "/static/media/lbm-all.d63522c5.png"
+  },
+  {
+    "revision": "8b1502347f379a9fe829e77bf11c8793",
+    "url": "/static/media/mapfinder-all.8b150234.png"
+  },
+  {
+    "revision": "1eb96f54d475e6763aa4f36265849ded",
+    "url": "/static/media/meal-planner.1eb96f54.png"
+  },
+  {
+    "revision": "20e34feb37f48b2a54efcdcdd906cfbb",
+    "url": "/static/media/ppeprotect.20e34feb.png"
+  },
+  {
+    "revision": "d50b836c39201c50ccc0b732982ba999",
+    "url": "/static/media/shop.d50b836c.png"
+  }
+]);
